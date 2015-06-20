9316982775c6ab3a59512c4940a45a5dac33617c
Paul Lamere <Paul.Lamere@gmail.com>
2015-06-20 09:59:28 +0200

    Added simplejson dependency

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     install_requires=['requests>=2.5'],
     install_requires=['pyen>=2.3'],
     install_requires=['spotipy>=2.0'],
+    install_requires=['simplejson>=3.6'],
     license='LICENSE.txt',
     packages=['pbl'])
